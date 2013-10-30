e85bdf8b244f4246f5155d8267132dd20a4f603d
Blake Bengtson <bbeng89@gmail.com>
2013-10-30 10:54:59 -0500

    Added credits

--- helpers/custom_text.php
+++ helpers/custom_text.php
@@ -1,7 +1,11 @@
 <?php
 defined('C5_EXECUTE') or die(_('Access Denied'));
 
-Loader::helper('text');
+/**
+ * @author 		Blake Bengtson (bbeng89)
+ * @copyright  	Copyright 2013 Blake Bengtson
+ * @license     concrete5.org marketplace license
+ */
 
 class CustomTextHelper extends TextHelper { 
 
